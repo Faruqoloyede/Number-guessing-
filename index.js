5d968e58ab62364d5f2ad3a15aca5fdec1b53ecb
Faruqoloyede <oloyedefaruq2@gmail.com>
2025-06-14 08:47:16 +0100

    added more functionalities

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const input = document.querySelector('.input');
 const guess_btn = document.querySelector('.guess_btn');
 const wrong = document.querySelector('.wrong');
 const playagain_btn = document.querySelector('.playagain_btn');
-const success_1 = document.querySelector('.success_1');
 const success_2 = document.querySelector('.success_2');
+const success = document.querySelector('.success');
 const error_text = document.querySelector('.error_text');
 const error = document.querySelector('.error');
 const try_btn = document.querySelector('.try_btn');
@@ -16,6 +16,7 @@ let max_attempt = 5;
 let attempt = 0
 
 
+
 const resetGame = ()=>{
     if(max_attempt === 0){
         wrapper.style.display='none';
@@ -25,14 +26,34 @@ const resetGame = ()=>{
     }
 }
 
+const playAgain = ()=>{
+    const guess = Math.floor(Math.random() * max) + min;
+    wrapper.style.display = 'block';
+    success.style.display = 'none';
+    error.style.display = 'none';
+    max_attempt = 5
+    info.textContent = `you have ${max_attempt} attempt remaining`
+    attempt = 0;
+    input.value = '';
+    wrong.textContent = ''
+    wrong.style.backgroundColor = '';
+
+}
+
+try_btn.addEventListener('click', playAgain);
+
+playagain_btn.addEventListener('click', playAgain)
+
 guess_btn.addEventListener('click', ()=>{
     const guess = Math.floor(Math.random() * max) + min;
     const value = input.value;
     const toNumber = Number(value);
     attempt++
     if(toNumber == guess){
-       console.log('yayy you get it');
-       console.log(`you get the number in ${attempt}`)
+      success.style.display = 'flex';
+      success_2.textContent = `you get it in ${attempt} guesses`;
+      wrapper.style.display = 'none';
+
     }else if(toNumber < guess){
         wrong.textContent = 'too low 😩try again'
         wrong.style.backgroundColor = '#FFCDD2';
@@ -49,3 +70,4 @@ guess_btn.addEventListener('click', ()=>{
 
     resetGame();
 })
+
