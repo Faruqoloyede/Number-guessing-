6810d36a000be6aaadc0734c3b21e0a8248e2080
Faruqoloyede <oloyedefaruq2@gmail.com>
2025-06-14 09:08:45 +0100

    added sound

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const error_text = document.querySelector('.error_text');
 const error = document.querySelector('.error');
 const try_btn = document.querySelector('.try_btn');
 const info = document.querySelector('.info');
+const win = new Audio('win.wav');
+const fail = new Audio('fail.mp3');
 
 const min = 1;
 const max = 10;
@@ -21,13 +23,15 @@ const resetGame = ()=>{
     if(max_attempt === 0){
         wrapper.style.display='none';
         error.style.display = 'flex'
+        fail.currentTime = 0;
+        fail.play();
     }else{
         return true
     }
 }
 
 const playAgain = ()=>{
-    const guess = Math.floor(Math.random() * max) + min;
+    const guess = Math.floor(Math.random() * (max - min +1)) + min;
     wrapper.style.display = 'block';
     success.style.display = 'none';
     error.style.display = 'none';
@@ -37,6 +41,11 @@ const playAgain = ()=>{
     input.value = '';
     wrong.textContent = ''
     wrong.style.backgroundColor = '';
+    win.pause();
+    win.currentTime = 0;
+    fail.pause();
+    win.currentTime = 0;
+    
 
 }
 
@@ -45,7 +54,8 @@ try_btn.addEventListener('click', playAgain);
 playagain_btn.addEventListener('click', playAgain)
 
 guess_btn.addEventListener('click', ()=>{
-    const guess = Math.floor(Math.random() * max) + min;
+    const guess = Math.floor(Math.random() * (max - min +1)) + min;
+    console.log(guess)
     const value = input.value;
     const toNumber = Number(value);
     attempt++
@@ -53,6 +63,8 @@ guess_btn.addEventListener('click', ()=>{
       success.style.display = 'flex';
       success_2.textContent = `you get it in ${attempt} guesses`;
       wrapper.style.display = 'none';
+      win.currentTime = 0;
+      win.play();
 
     }else if(toNumber < guess){
         wrong.textContent = 'too low 😩try again'
